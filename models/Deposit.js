08a102a8cb8afc5d235bbc8149c15dd4f133d7f4
senam9677-oss <senam9677@gmail.com>
2026-08-17 10:14:33 +0000

    Update Deposit.js

--- models/Deposit.js
+++ models/Deposit.js
@@ -2,20 +2,11 @@ const mongoose = require("mongoose");
 
 const depositSchema = new mongoose.Schema(
   {
-    // ==========================================
-    // USER
-    // ==========================================
-
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
-
-
-    // ==========================================
-    // DEPOSIT AMOUNT
-    // ==========================================
 
     amount: {
       type: Number,
@@ -23,20 +14,10 @@ const depositSchema = new mongoose.Schema(
       min: 100,
     },
 
-
-    // ==========================================
-    // PAYMENT METHOD
-    // ==========================================
-
     paymentMethod: {
       type: String,
       required: true,
     },
-
-
-    // ==========================================
-    // TRANSACTION ID
-    // ==========================================
 
     transactionId: {
       type: String,
@@ -44,54 +25,16 @@ const depositSchema = new mongoose.Schema(
       trim: true,
     },
 
-
-    // ==========================================
-    // SENDER NAME
-    // ==========================================
-
-    senderName: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-
-
-    // ==========================================
-    // SENDER PHONE
-    // ==========================================
-
-    senderPhone: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-
-
-    // ==========================================
-    // DEPOSIT STATUS
-    // ==========================================
-
     status: {
       type: String,
-      enum: [
-        "Pending",
-        "Approved",
-        "Rejected"
-      ],
+      enum: ["Pending", "Approved", "Rejected"],
       default: "Pending",
     },
   },
-
-
-  // ==========================================
-  // TIMESTAMPS
-  // ==========================================
-
   {
     timestamps: true,
   }
 );
-
 
 module.exports =
   mongoose.models.Deposit ||
